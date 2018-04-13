980e5b63dc4d9c69eb42b89a0b49df5530afe651
Vadim <calius.ligarius@gmail.com>
2018-04-14 01:54:20 +0300

    fix the HTTPError 404 not found

--- API_request_to_TMDb/API_requests.py
+++ API_request_to_TMDb/API_requests.py
@@ -21,17 +21,24 @@ def make_tmdb_api_request(method, api_key, extra_params=None):
 
 list_of_films = []
 for i in range(1000):
-    string = make_tmdb_api_request(method='/movie/' + str(i+10000), api_key='4aac4d6948ada9b037fddc6466d965be')
-    data = {}
-    data['original_title'] = string['original_title']
-    data['original_language'] = string['original_language']
-    data['budget'] = string['budget']
-    data['production_countries'] = string['production_countries']
-    data['release_date'] = string['release_date']
-    data['runtime'] = string['runtime']
-    data['spoken_languages'] = string['spoken_languages']
-    list_of_films.append(data)
+    try:
+        string = make_tmdb_api_request(method='/movie/' + str(i+10000), api_key='4aac4d6948ada9b037fddc6466d965be')
+        data = {}
+        data['original_title'] = string['original_title']
+        data['original_language'] = string['original_language']
+        data['budget'] = string['budget']
+        data['production_countries'] = string['production_countries']
+        data['release_date'] = string['release_date']
+        data['runtime'] = string['runtime']
+        data['spoken_languages'] = string['spoken_languages']
+        list_of_films.append(data)
+    except urllib.error.HTTPError as err:
+        if err.code == 404:
+            continue
+        else:
+            raise err
+    if i % 10 == 0:
+        print(i)
+
 with open('film_base.json', 'w') as inFile:
     json.dump(list_of_films, inFile, indent = 4)
-
-    
